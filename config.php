3f5b2d57e3f08f6d43700ed664f0a3e714c9aa26
Theodore <theonastos@yahoo.com>
2018-11-29 11:22:13 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', 'Thodoris7');
+	define('DB_PASSWORD', '123');
 	define('DB_DATABASE', 'web_store');
 	
 	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
